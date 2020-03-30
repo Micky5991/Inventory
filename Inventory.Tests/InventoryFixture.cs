ba4c0977213bd45535c77c7c9dfa493fd91be04d
Francesco Paolocci <micky5991@gmail.com>
2020-03-30 14:31:23 +0200

    Add property notification testing of mutable properties

--- Inventory.Tests/InventoryFixture.cs
+++ Inventory.Tests/InventoryFixture.cs
@@ -326,6 +326,56 @@ namespace Micky5991.Inventory.Tests
             _itemMock.Verify(x => x.SetCurrentInventory(It.IsAny<IInventory>()), Times.Never);
         }
 
+        [TestMethod]
+        public async Task AddingItemWillNotifyUsedCapacityAndAvailableCapacity()
+        {
+            using var monitoredInventory = _inventory.Monitor();
+
+            await AddItemToInventoryAsync(10);
+
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.UsedCapacity);
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
+        [TestMethod]
+        public async Task RemovingItemWillNotifyUsedCapacityAndAvailableCapacity()
+        {
+            var item = await AddItemToInventoryAsync(10);
+
+            using var monitoredInventory = _inventory.Monitor();
+
+            await _inventory.RemoveItemAsync(item);
+
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.UsedCapacity);
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
+        [TestMethod]
+        public async Task ChangingCapacityOfInventoryWillNotify()
+        {
+            var item = await AddItemToInventoryAsync(10);
+
+            using var monitoredInventory = _inventory.Monitor();
+
+            _inventory.SetCapacity(20);
+
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.Capacity);
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
+        [TestMethod]
+        public async Task SettingCapacityOfInventoryWontNotify()
+        {
+            await AddItemToInventoryAsync(10);
+
+            using var monitoredInventory = _inventory.Monitor();
+
+            _inventory.SetCapacity(_inventory.Capacity);
+
+            monitoredInventory.Should().NotRaisePropertyChangeFor(x => x.Capacity);
+            monitoredInventory.Should().NotRaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
         private async Task<FakeItem> AddItemToInventoryAsync(int weight = 10)
         {
             var item = new FakeItem(weight);
