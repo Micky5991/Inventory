b72497547022d97f9a184b869c18e00bf9821315
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 14:26:29 +0200

    Move inventory capacity altering and item movement checks

--- Inventory.Tests/InventoryFixture.Capacity.cs
+++ Inventory.Tests/InventoryFixture.Capacity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -30,5 +31,72 @@ namespace Micky5991.Inventory.Tests
 
             AssertInventoryCapacity(0, capacity, inventory);
         }
+
+        [TestMethod]
+        public void ChangingCapacityBelowMinimalCapacityWillThrowException()
+        {
+            Action act = () => _inventory.SetCapacity(Entities.Inventory.Inventory.MinimalInventoryCapacity - 1);
+
+            act.Should().Throw<ArgumentOutOfRangeException>()
+                .Where(x => x.Message.Contains($"{Entities.Inventory.Inventory.MinimalInventoryCapacity} or higher"));
+        }
+
+        [TestMethod]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity)]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity + 1)]
+        [DataRow(int.MaxValue)]
+        public void ChangingCapacityWillChangeCapacityCorrectly(int capacity)
+        {
+            _inventory.SetCapacity(capacity);
+
+            AssertInventoryCapacity(0, capacity);
+        }
+
+        [TestMethod]
+        public async Task SettingCapacityBelowUsedCapacityWillReturnFalse()
+        {
+            await AddItemToInventoryAsync(50);
+
+            _inventory.SetCapacity(51).Should().BeTrue();
+            _inventory.SetCapacity(50).Should().BeTrue();
+            _inventory.SetCapacity(49).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ChangingCapacityWillAlterAvailableCapacity()
+        {
+            var oldCapacity = _inventory.Capacity;
+            var newCapacity = _inventory.Capacity - 10;
+
+            _inventory.SetCapacity(newCapacity);
+
+            _inventory.AvailableCapacity
+                .Should().Be(newCapacity)
+                .And.BeLessThan(oldCapacity);
+        }
+
+        [TestMethod]
+        public async Task ChangingCapacityBelowUsedCapacityWillKeepValueSame()
+        {
+            await AddItemToInventoryAsync(50);
+
+            var oldCapacity = _inventory.Capacity;
+
+            _inventory.SetCapacity(30).Should().BeFalse();
+            _inventory.Capacity.Should().Be(oldCapacity);
+        }
+
+        [TestMethod]
+        public async Task ChangingCapacityWillKeepUsedCapacitySame()
+        {
+            await AddItemToInventoryAsync(50);
+
+            var oldUsedCapacity = _inventory.UsedCapacity;
+
+            _inventory.SetCapacity(60);
+
+            _inventory.UsedCapacity.Should().Be(oldUsedCapacity);
+        }
+
     }
 }
