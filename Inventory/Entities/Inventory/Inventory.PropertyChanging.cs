da8e7a099a09157222185e99f65f64bbec84447b
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 20:09:21 +0200

    Reduce compiler warning amount

--- Inventory/Entities/Inventory/Inventory.PropertyChanging.cs
+++ Inventory/Entities/Inventory/Inventory.PropertyChanging.cs
@@ -9,7 +9,7 @@ namespace Micky5991.Inventory.Entities.Inventory
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
