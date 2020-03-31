156b97333cdf1a48f15c22fb7dbfc75ae6573a8f
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 14:26:29 +0200

    Move inventory capacity altering and item movement checks

--- Inventory.Tests/InventoryFixture.InsertRemove.cs
+++ Inventory.Tests/InventoryFixture.InsertRemove.cs
@@ -1,7 +1,101 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Micky5991.Inventory.Exceptions;
+using Micky5991.Inventory.Tests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Micky5991.Inventory.Tests
 {
     public partial class InventoryFixture
     {
+        [TestMethod]
+        public async Task RemovingNullFromInventoryWillThrowException()
+        {
+            Func<Task> act = () => _inventory.RemoveItemAsync(null);
 
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public async Task InsertingNullFromInventoryWillThrowException()
+        {
+            Func<Task> act = () => _inventory.InsertItemAsync(null);
+
+            (await act.Should().ThrowAsync<ArgumentNullException>())
+                .Where(x => string.IsNullOrWhiteSpace(x.Message) == false);
+        }
+
+        [TestMethod]
+        public async Task InsertedItemsWillAppearInItemsList()
+        {
+            var item = await AddItemToInventoryAsync();
+
+            _inventory.Items.Should()
+                .ContainSingle(x => x.Key == item.RuntimeId && x.Value == item);
+        }
+
+        [TestMethod]
+        public async Task InsertingItemMultipleTimesWillNotChangeDictionaryLength()
+        {
+            var item = await AddItemToInventoryAsync();
+
+            await _inventory.InsertItemAsync(item);
+
+            _inventory.Items.Should()
+                .ContainSingle(x => x.Key == item.RuntimeId && x.Value == item);
+        }
+
+        [TestMethod]
+        public async Task InsertingItemToInventoryTwiceWillMarkFollowingCallsAsUnsuccessful()
+        {
+            var item = new FakeItem(10);
+
+            (await _inventory.InsertItemAsync(item)).Should().BeTrue();
+            (await _inventory.InsertItemAsync(item)).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public async Task RemovingItemFromInventoryWillReturnFalseIfNotFound()
+        {
+            var item = await AddItemToInventoryAsync();
+
+            (await _inventory.RemoveItemAsync(item)).Should().BeTrue();
+            (await _inventory.RemoveItemAsync(item)).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public async Task RemovingItemFromInventoryThatWasNotAddedWillReturnFalse()
+        {
+            var item = new FakeItem(10);
+
+            (await _inventory.RemoveItemAsync(item)).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public async Task AddingItemWithHigherWeightThanCapacityWillThrowException()
+        {
+            var item = new FakeItem(InventoryCapacity + 1);
+
+            Func<Task> act = () => _inventory.InsertItemAsync(item);
+
+            (await act.Should().ThrowAsync<InventoryCapacityException>())
+                .Where(x =>
+                            x.Message.Contains(item.Handle) &&
+                            x.Message.Contains(item.RuntimeId.ToString())
+                            );
+        }
+
+        [TestMethod]
+        public async Task AddingInvalidItemWillNotAddItemToInventory()
+        {
+            var item = new FakeItem(InventoryCapacity + 1);
+
+            Func<Task> act = () => _inventory.InsertItemAsync(item);
+
+            await act.Should().ThrowAsync<InventoryCapacityException>();
+
+            _inventory.Items.Should().BeEmpty();
+        }
     }
 }
