a2c0f270f7dc411dcdd5011c8b0acf36986c17b3
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 14:17:17 +0200

    Add example filling of inventory with itemfactory

--- Inventory.Example/Services/InventoryBuilderService.cs
+++ Inventory.Example/Services/InventoryBuilderService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Micky5991.Inventory.Interfaces;
 
@@ -18,14 +19,36 @@ namespace Inventory.Example.Services
 
         public async Task SetupInventoryAsync()
         {
-            _inventory = _inventoryFactory.CreateInventory(5);
+            _inventory = _inventoryFactory.CreateInventory(10);
 
             await FillInventoryAsync();
         }
 
         private async Task FillInventoryAsync()
         {
-            var item = _itemFactory.CreateItem(ItemHandle.Apple.ToString(), 1);
+            Console.WriteLine("--> Add 1 apple and 3 water to inventory, expect 1 apple and 3 water");
+
+            var apple = _itemFactory.CreateItem(ItemHandle.Apple.ToString(), 1);
+            var water = _itemFactory.CreateItem(ItemHandle.Water.ToString(), 3);
+
+            await _inventory.InsertItemAsync(apple);
+            await _inventory.InsertItemAsync(water);
+
+            foreach (var item in _inventory.Items.Values)
+            {
+                Console.WriteLine($"Item: {item.Handle} - {item.GetType()} - {item.Amount}x");
+            }
+
+            Console.WriteLine("--> Add another 2 water into the inventory, expect 5 water.");
+
+            var additionalWater = _itemFactory.CreateItem(ItemHandle.Water.ToString(), 2);
+
+            await _inventory.InsertItemAsync(additionalWater);
+
+            foreach (var item in _inventory.Items.Values)
+            {
+                Console.WriteLine($"Item: {item.Handle} - {item.GetType()} - {item.Amount}x");
+            }
         }
 
     }
