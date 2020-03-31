1923086eef0a2e6b5476787b47f2060c8241aa5b
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 14:24:27 +0200

    Move Inventory creation unit-tests

--- Inventory.Tests/InventoryFixture.cs
+++ Inventory.Tests/InventoryFixture.cs
@@ -27,30 +27,6 @@ namespace Micky5991.Inventory.Tests
         }
 
         [TestMethod]
-        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity - 1)]
-        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity - 2)]
-        [DataRow(int.MinValue)]
-        public void CreatingInventoryWithInvalidCapacityThrowsException(int capacity)
-        {
-            Action act = () => new Entities.Inventory.Inventory(capacity);
-
-            act.Should().Throw<ArgumentOutOfRangeException>()
-                .Where(x => x.Message.Contains($"{Entities.Inventory.Inventory.MinimalInventoryCapacity} or higher"));
-        }
-
-        [TestMethod]
-        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity)]
-        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity + 1)]
-        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity + 2)]
-        [DataRow(int.MaxValue)]
-        public void CreatingInventoryWithCapacityWillSetCapacityValues(int capacity)
-        {
-            var inventory = new Entities.Inventory.Inventory(capacity);
-
-            AssertInventoryCapacity(0, capacity, inventory);
-        }
-
-        [TestMethod]
         public async Task InsertingItemToInventoryWillChangeCapacity()
         {
             await AddItemToInventoryAsync(10);
