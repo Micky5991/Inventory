9f3fb506008f5152dee0374dbe302125c30d49c7
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 14:28:20 +0200

    Fix location of creation tests to right partial

--- Inventory.Tests/InventoryFixture.Creation.cs
+++ Inventory.Tests/InventoryFixture.Creation.cs
@@ -1,7 +1,33 @@
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Micky5991.Inventory.Tests
 {
     public partial class InventoryFixture
     {
+        [TestMethod]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity - 1)]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity - 2)]
+        [DataRow(int.MinValue)]
+        public void CreatingInventoryWithInvalidCapacityThrowsException(int capacity)
+        {
+            Action act = () => new Entities.Inventory.Inventory(capacity);
 
+            act.Should().Throw<ArgumentOutOfRangeException>()
+                .Where(x => x.Message.Contains($"{Entities.Inventory.Inventory.MinimalInventoryCapacity} or higher"));
+        }
+
+        [TestMethod]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity)]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity + 1)]
+        [DataRow(Entities.Inventory.Inventory.MinimalInventoryCapacity + 2)]
+        [DataRow(int.MaxValue)]
+        public void CreatingInventoryWithCapacityWillSetCapacityValues(int capacity)
+        {
+            var inventory = new Entities.Inventory.Inventory(capacity);
+
+            AssertInventoryCapacity(0, capacity, inventory);
+        }
     }
 }
