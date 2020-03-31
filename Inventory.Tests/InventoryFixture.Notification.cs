c8ad407410729c4104950da3ddaadd84cc10c3d1
Francesco Paolocci <micky5991@gmail.com>
2020-03-31 14:27:08 +0200

    Move property notifications to dedicated partial

--- Inventory.Tests/InventoryFixture.Notification.cs
+++ Inventory.Tests/InventoryFixture.Notification.cs
@@ -1,7 +1,59 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Micky5991.Inventory.Tests
 {
     public partial class InventoryFixture
     {
+        [TestMethod]
+        public async Task AddingItemWillNotifyUsedCapacityAndAvailableCapacity()
+        {
+            using var monitoredInventory = _inventory.Monitor();
 
+            await AddItemToInventoryAsync(10);
+
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.UsedCapacity);
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
+        [TestMethod]
+        public async Task RemovingItemWillNotifyUsedCapacityAndAvailableCapacity()
+        {
+            var item = await AddItemToInventoryAsync(10);
+
+            using var monitoredInventory = _inventory.Monitor();
+
+            await _inventory.RemoveItemAsync(item);
+
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.UsedCapacity);
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
+        [TestMethod]
+        public async Task ChangingCapacityOfInventoryWillNotify()
+        {
+            var item = await AddItemToInventoryAsync(10);
+
+            using var monitoredInventory = _inventory.Monitor();
+
+            _inventory.SetCapacity(20);
+
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.Capacity);
+            monitoredInventory.Should().RaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
+
+        [TestMethod]
+        public async Task SettingCapacityOfInventoryWontNotify()
+        {
+            await AddItemToInventoryAsync(10);
+
+            using var monitoredInventory = _inventory.Monitor();
+
+            _inventory.SetCapacity(_inventory.Capacity);
+
+            monitoredInventory.Should().NotRaisePropertyChangeFor(x => x.Capacity);
+            monitoredInventory.Should().NotRaisePropertyChangeFor(x => x.AvailableCapacity);
+        }
     }
 }
