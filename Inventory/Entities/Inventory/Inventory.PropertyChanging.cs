4aee4e1de55885e3b0738896173fe47fbdad7767
Francesco Paolocci <micky5991@gmail.com>
2020-04-10 22:20:29 +0200

    Add missing documentation for Inventory class

--- Inventory/Entities/Inventory/Inventory.PropertyChanging.cs
+++ Inventory/Entities/Inventory/Inventory.PropertyChanging.cs
@@ -4,11 +4,18 @@ using JetBrains.Annotations;
 
 namespace Micky5991.Inventory.Entities.Inventory
 {
+    /// <content>
+    /// Implementation of <see cref="INotifyPropertyChanged"/> interface.
+    /// </content>
     public partial class Inventory
     {
         /// <inheritdoc />
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// Invocator for the interface <see cref="INotifyPropertyChanged"/>.
+        /// </summary>
+        /// <param name="propertyName">Name of the property that called this method.</param>
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
