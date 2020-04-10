aa7b89ff219c47d43b016a5dd7330e943877aa9c
Francesco Paolocci <micky5991@gmail.com>
2020-04-10 20:49:48 +0200

    Improve method order and documentation in Item

--- Inventory/ItemMeta.cs
+++ Inventory/ItemMeta.cs
@@ -4,44 +4,22 @@ using Micky5991.Inventory.Interfaces;
 
 namespace Micky5991.Inventory
 {
+    /// <summary>
+    /// This type gives basic information about an item without creation.
+    /// </summary>
     public class ItemMeta
     {
         /// <summary>
-        /// Unique handle of this meta information
+        /// Initializes a new instance of the <see cref="ItemMeta"/> class.
         /// </summary>
-        public string Handle { get; }
-
-        /// <summary>
-        /// Implementation type of the item this meta will be represented by.
-        /// </summary>
-        public Type Type { get; }
-
-        /// <summary>
-        /// Default display name of the created item
-        /// </summary>
-        public string DisplayName { get; }
-
-        /// <summary>
-        /// Weight of the final item
-        /// </summary>
-        public int DefaultWeight { get; }
-
-        /// <summary>
-        /// Flag collection that represent different item abilities
-        /// </summary>
-        public ItemFlags Flags { get; }
-
-        /// <summary>
-        /// Creates an instance of <see cref="ItemMeta"/> that gives basic information about an item without creation.
-        /// </summary>
-        /// <param name="handle">Unique handle of item</param>
-        /// <param name="type">Underlying type of item that this item will be implemented of</param>
-        /// <param name="displayName">Default display name this item should have</param>
+        /// <param name="handle">Unique handle of item.</param>
+        /// <param name="type">Underlying type of item that this item will be implemented of.</param>
+        /// <param name="displayName">Default display name this item should have.</param>
         /// <param name="defaultWeight">Default item weight of this item.</param>
-        /// <param name="flags">Behaviour flags of this item</param>
-        /// <exception cref="ArgumentException"><paramref name="type"/> is invalid</exception>
-        /// <exception cref="ArgumentNullException"><paramref name="handle"/> or <paramref name="type"/> is null</exception>
-        /// <exception cref="ArgumentOutOfRangeException"><paramref name="defaultWeight"/> is 0 or lower</exception>
+        /// <param name="flags">Behaviour flags of this item.</param>
+        /// <exception cref="ArgumentException"><paramref name="type"/> is invalid.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="handle"/> or <paramref name="type"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="defaultWeight"/> is 0 or lower.</exception>
         public ItemMeta(string handle, Type type, string displayName, int defaultWeight, ItemFlags flags = ItemFlags.None)
         {
             if (string.IsNullOrWhiteSpace(handle))
@@ -75,5 +53,30 @@ namespace Micky5991.Inventory
             this.DefaultWeight = defaultWeight;
             this.Flags = flags;
         }
+
+        /// <summary>
+        /// Gets the unique handle of this meta information.
+        /// </summary>
+        public string Handle { get; }
+
+        /// <summary>
+        /// Gets the implementation type of the item this meta will be represented by.
+        /// </summary>
+        public Type Type { get; }
+
+        /// <summary>
+        /// Gets the default display name of the created item.
+        /// </summary>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Gets the weight of the final item.
+        /// </summary>
+        public int DefaultWeight { get; }
+
+        /// <summary>
+        /// Gets the flag collection that represent different item abilities.
+        /// </summary>
+        public ItemFlags Flags { get; }
     }
 }
