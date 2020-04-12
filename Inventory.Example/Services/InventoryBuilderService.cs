439ba65bb2d9d43a5fa521b0c6e307d6a22afa0c
Francesco Paolocci <micky5991@gmail.com>
2020-04-12 21:22:58 +0200

    Fix field and property naming in example project

--- Inventory.Example/Services/InventoryBuilderService.cs
+++ Inventory.Example/Services/InventoryBuilderService.cs
@@ -7,20 +7,20 @@ namespace Inventory.Example.Services
 {
     public class InventoryBuilderService
     {
-        private readonly IInventoryFactory _inventoryFactory;
-        private readonly IItemFactory _itemFactory;
+        private readonly IInventoryFactory inventoryFactory;
+        private readonly IItemFactory itemFactory;
 
-        private IInventory _inventory;
+        private IInventory inventory;
 
         public InventoryBuilderService(IInventoryFactory inventoryFactory, IItemFactory itemFactory)
         {
-            _inventoryFactory = inventoryFactory;
-            _itemFactory = itemFactory;
+            this.inventoryFactory = inventoryFactory;
+            this.itemFactory = itemFactory;
         }
 
         public void SetupInventory()
         {
-            _inventory = _inventoryFactory.CreateInventory(10);
+            this.inventory = this.inventoryFactory.CreateInventory(10);
 
             this.FillInventory();
         }
@@ -29,35 +29,35 @@ namespace Inventory.Example.Services
         {
             Console.WriteLine("--> Add 1 apple and 3 water to inventory, expect 1 apple and 3 water");
 
-            var apple = _itemFactory.CreateItem(ItemHandle.Apple, 1);
-            var water = _itemFactory.CreateItem(ItemHandle.Water, 3);
+            var apple = this.itemFactory.CreateItem(ItemHandle.Apple, 1);
+            var water = this.itemFactory.CreateItem(ItemHandle.Water, 3);
 
-            _inventory.InsertItem(apple);
-            _inventory.InsertItem(water);
+            this.inventory.InsertItem(apple);
+            this.inventory.InsertItem(water);
 
-            foreach (var item in _inventory.Items)
+            foreach (var item in this.inventory.Items)
             {
                 Console.WriteLine($"Item: {item.Handle} - {item.GetType()} - {item.Amount}x");
             }
 
             Console.WriteLine("--> Add another 2 water into the inventory, expect 5 water.");
 
-            var additionalWater = _itemFactory.CreateItem(ItemHandle.Water, 2);
+            var additionalWater = this.itemFactory.CreateItem(ItemHandle.Water, 2);
 
-            _inventory.InsertItem(additionalWater);
+            this.inventory.InsertItem(additionalWater);
 
-            foreach (var item in _inventory.Items)
+            foreach (var item in this.inventory.Items)
             {
                 Console.WriteLine($"Item: {item.Handle} - {item.GetType()} - {item.Amount}x");
             }
 
             Console.WriteLine("Take 2 Water from inventory and print seperate");
 
-            var currentWater = _inventory.GetItems(ItemHandle.Water).First();
+            var currentWater = this.inventory.GetItems(ItemHandle.Water).First();
             var splitWater = currentWater.SplitItem(2);
 
             Console.WriteLine("[INVENTORY]");
-            foreach (var item in _inventory.Items)
+            foreach (var item in this.inventory.Items)
             {
                 Console.WriteLine($"Item: {item.Handle} - {item.GetType()} - {item.Amount}x");
             }
