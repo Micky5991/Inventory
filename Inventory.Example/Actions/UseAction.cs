16b5cb65eca615fc5a9a192cb84ac1996e034511
Francesco Paolocci <micky5991@gmail.com>
2020-04-13 18:27:45 +0200

    Implement example action to water item

---
+++ Inventory.Example/Actions/UseAction.cs
@@ -0,0 +1,29 @@
+using System;
+using Micky5991.Inventory.Data;
+using Micky5991.Inventory.Entities.Actions;
+
+namespace Inventory.Example.Actions
+{
+    public class UseAction : ItemActionBase<OutgoingItemActionData, IncomingItemActionData>
+    {
+        private readonly string displayName;
+
+        private readonly Action callback;
+
+        public UseAction(string displayName, Action callback)
+        {
+            this.displayName = displayName;
+            this.callback = callback;
+        }
+
+        public override void Execute(IncomingItemActionData data)
+        {
+            this.callback();
+        }
+
+        public override OutgoingItemActionData BuildActionData()
+        {
+            return new OutgoingItemActionData(this.RuntimeId);
+        }
+    }
+}
--- Inventory.Example/Items/WaterItem.cs
+++ Inventory.Example/Items/WaterItem.cs
@@ -1,12 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Inventory.Example.Actions;
 using Micky5991.Inventory;
 using Micky5991.Inventory.AggregatedServices;
+using Micky5991.Inventory.Data;
+using Micky5991.Inventory.Entities.Item.Subtypes;
+using Micky5991.Inventory.Interfaces;
 
 namespace Inventory.Example.Items
 {
-    public class WaterItem : BaseItem
+    public class WaterItem : ActionableItem<OutgoingItemActionData, IncomingItemActionData>
     {
         public WaterItem(ItemMeta meta, AggregatedItemServices itemServices) : base(meta, itemServices)
         {
+        }
+
+        protected override IEnumerable<IItemAction<OutgoingItemActionData, IncomingItemActionData>> RegisterAllActions()
+        {
+            yield return new UseAction("Drink water", OnDrinkItem);
+        }
+
+        private void OnDrinkItem()
+        {
+            Console.WriteLine("Item has been consumed");
         }
     }
 }
